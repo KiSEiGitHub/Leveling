403d6f58949d4d79c93eed06a215a8edd7a0eb9c
Tom <tommusique974@gmail.com>
2022-05-10 13:38:28 +0200

    opti

--- Connexion.php
+++ Connexion.php
@@ -15,13 +15,7 @@ $setup = new setup();
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Leveling - Page de connexion</title>
-
-    <!--  CSS  -->
     <link rel="stylesheet" href="scss/styles.css">
 </head>
 
--- index.php
+++ index.php
@@ -22,15 +22,16 @@ $setup = new setup();
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Leveling</title>
-
-    <!--  CSS  -->
+    <!-- style -->
     <link rel="stylesheet" href="scss/styles.css">
+
+    <!-- CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <title>Leveling</title>
 </head>
+
 <body>
 <h3>Bonjour <?= $_SESSION['pseudo'] ?></h3>
 
@@ -41,12 +42,8 @@ $setup = new setup();
 <a href="search.php">search</a>
 <a href="insert_games.php">ajouter un jeu</a>
 
+<!-- Javascript -->
 <script src="js/main.js"></script>
-<!-- BARRE DE RECHERCHE -->
-
-
-<script src="js/main.js"></script>
-<!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossorigin="anonymous"></script>
