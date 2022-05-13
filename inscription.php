df5942311399e6447092273c610f717be9237c5d
Clara Garcia <c.garcia@cfa-insta.fr>
2022-05-13 23:04:49 +0200

    page connexion et inscription terminée + ajout de la navbar sur toutes les pages

--- inscription.php
+++ inscription.php
@@ -22,113 +22,126 @@ $setup = new setup();
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&display=swap');
     </style>
-    <title>Inscription</title>
     <link rel="stylesheet" href="scss/styles.css">
+    <title>Inscription</title>
 </head>
 
 <body>
-<div id="green-bar">
-    <img src="images/leveling-logo.png" alt="" width="200em">
-</div>
-<main class="form-inscription">
-    <div class="inscription-form">
-        <form method="post" action="#" enctype="multipart/form-data">
-            <div class="profile-picture">
-                <div class="user">
-                    <img src="images/user.png" alt="">
-                    <label>
-                        <input type="file" name="img"/>
-                    </label>               
-                </div>
-            </div>
-
-            <div class="form-floating">
-                <label for="pseudo">Pseudo
-                    <input type="text" class="form-control" id="floatingInput" name="pseudo">
-                </label>
-            </div>
-
-            <div class="form-floating">
-                <label for="mdp">Mot de passe
-                    <input type="password" class="form-control" id="floatingPassword" name="mdp">
-                </label>
-            </div>
-
-            <div class="form-floating">
-                <label for="prenom">Prénom
-                    <input type="text" class="form-control" id="floatingPassword" name="prenom">
-                </label>
-            </div>
-
-            <div class="form-floating">
-                <label for="nom">Nom
-                    <input type="text" class="form-control" id="floatingPassword" name="nom">
-                </label>
-            </div>
-
-
-            <div class="form-floating">
-                <label for="dateNaissance">Date de naissance
-                    <input type="date" class="form-control" id="floatingPassword" name="dateNaissance">
-                </label>
-            </div>
-
-            <div class="form-floating">
-                <label for="age">Age
-                    <input type="number" class="form-control" id="floatingPassword" name="age">
-                </label>
-            </div>
-
-            <div class="form-floating">
-                <label for="mail">E-mail
-                    <input type="email" class="form-control" id="floatingPassword" name="mail">
-                </label>
-            </div>
-
-
-            <div class="form-floating">
-                <label for="bio">Bio
-                    <input type="text" class="form-control" id="floatingPassword" name="bio">
-                </label>
-            </div>
-
-            <input type="submit" name="btn" value="S'inscrire">
-
-            <div class="inscription">
-                <span>
-                    Déjà inscrit ?&nbsp;
-                    <a href="Connexion.php">Connexion</a>
-                </span>
-
-            </div>
+    <div id="green-bar">
+        <h1>LEVELING</h1>
+        <div class="nav-icons">
+            <input type="text" name="search" placeholder="Rechercher" id="search">
             <?php
-            if (isset($_POST['btn']) && isset($_FILES['img'])) {
-                if (
-                    !empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['mail'])
-                    && !empty($_POST['mdp']) && !empty($_POST['bio']) && !empty($_POST['pseudo'])
-                    && !empty($_POST['age'] && !empty($_POST['dateNaissance']) && !empty($_FILES['img']))
-                ) {
-                    if ($_FILES['img']['name'] == '') {
-                        echo "<p class='error'>Veuillez ajouté une image de profil</p>";
-                    } else {
-                        $newimg = $setup->FakeImage($_FILES['img'],"./assets/img/UserProfilePicture/");
-
-                        if ($newimg === "err") {
-                            echo "veuillez insérer une image valide";
-                        } else {
-                            $controler->insertUser($_POST, $newimg);
-                        }
-                    }
-
-                } else {
-                    echo "<p class='error'>Veuillez renseigner tous les champs</p>";
-                }
-            }
+            if (isset($_SESSION['pseudo'])) {
             ?>
-        </form>
-
+                <a href="./profil.php"><img src="assets/img/UserProfilePicture/<?= $user['img'] ?>" class="nav-user alt=" pfp"></a>
+            <?php
+            } else { ?>
+                <a href="./profil.php"><img class="nav-user" src="./images/user-circle.png" alt=""></a>
+            <?php } ?>
+            <a href="./settings.php"><img class="nav-user" src="./images/settings.png" alt=""></a>
+        </div>
     </div>
-</main>
-</body>
-</html>
 
+
+    <main class="form-inscription">
+        <div class="inscription-form">
+            <form method="post" action="#" enctype="multipart/form-data">
+                <div class="profile-picture">
+                    <div class="user">
+                        <img src="images/user.png" alt="">
+                        <label>
+                            <input type="file" name="img" />
+                        </label>
+                    </div>
+                </div>
+
+                <div class="form-floating">
+                    <label for="pseudo">Pseudo
+                        <input type="text" class="form-control" id="floatingInput" name="pseudo">
+                    </label>
+                </div>
+
+                <div class="form-floating">
+                    <label for="mdp">Mot de passe
+                        <input type="password" class="form-control" id="floatingPassword" name="mdp">
+                    </label>
+                </div>
+
+                <div class="form-floating">
+                    <label for="prenom">Prénom
+                        <input type="text" class="form-control" id="floatingPassword" name="prenom">
+                    </label>
+                </div>
+
+                <div class="form-floating">
+                    <label for="nom">Nom
+                        <input type="text" class="form-control" id="floatingPassword" name="nom">
+                    </label>
+                </div>
+
+
+                <div class="form-floating">
+                    <label for="dateNaissance">Date de naissance
+                        <input type="date" class="form-control" id="floatingPassword" name="dateNaissance">
+                    </label>
+                </div>
+
+                <div class="form-floating">
+                    <label for="age">Age
+                        <input type="number" class="form-control" id="floatingPassword" name="age">
+                    </label>
+                </div>
+
+                <div class="form-floating">
+                    <label for="mail">E-mail
+                        <input type="email" class="form-control" id="floatingPassword" name="mail">
+                    </label>
+                </div>
+
+
+                <div class="form-floating-bio">
+                    <label for="bio">Bio
+                        <input type="text" class="form-control" id="floatingPassword" name="bio" rows="3">
+                    </label>
+                </div>
+
+                <input type="submit" name="btn" value="S'inscrire">
+
+                <div class="inscription">
+                    <span>
+                        Déjà inscrit ?&nbsp;
+                        <a href="Connexion.php">Connexion</a>
+                    </span>
+
+                </div>
+                <?php
+                if (isset($_POST['btn']) && isset($_FILES['img'])) {
+                    if (
+                        !empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['mail'])
+                        && !empty($_POST['mdp']) && !empty($_POST['bio']) && !empty($_POST['pseudo'])
+                        && !empty($_POST['age'] && !empty($_POST['dateNaissance']) && !empty($_FILES['img']))
+                    ) {
+                        if ($_FILES['img']['name'] == '') {
+                            echo "<p class='error'>Veuillez ajouté une image de profil</p>";
+                        } else {
+                            $newimg = $setup->FakeImage($_FILES['img'], "./assets/img/UserProfilePicture/");
+
+                            if ($newimg === "err") {
+                                echo "veuillez insérer une image valide";
+                            } else {
+                                $controler->insertUser($_POST, $newimg);
+                            }
+                        }
+                    } else {
+                        echo "<p class='error'>Veuillez renseigner tous les champs</p>";
+                    }
+                }
+                ?>
+            </form>
+
+        </div>
+    </main>
+</body>
+
+</html>
---
+++ profil_jeux.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&display=swap');
+    </style>
+    <link rel="stylesheet" href="scss/styles.css">
+    <title>Jeux</title>
+</head>
+
+<body>
+
+    <div id="green-bar">
+        <h1>LEVELING</h1>
+        <div class="nav-icons">
+            <input type="text" name="search" placeholder="Rechercher" id="search">
+            <?php
+            if (isset($_SESSION['pseudo'])) {
+            ?>
+                <a href="./profil.php"><img src="assets/img/UserProfilePicture/<?= $user['img'] ?>" class="nav-user alt=" pfp"></a>
+            <?php
+            } else { ?>
+                <a href="./profil.php"><img class="nav-user" src="./images/user-circle.png" alt=""></a>
+            <?php } ?>
+            <a href="./settings.php"><img class="nav-user" src="./images/settings.png" alt=""></a>
+        </div>
+    </div>
+
+</html>
