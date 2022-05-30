824b9c345c875ac6676b4d696499e30b4ae12490
Tom <tommusique974@gmail.com>
2022-05-30 18:45:53 +0200

    prepare

--- profil_settings.php
+++ profil_settings.php
@@ -31,83 +31,91 @@ $ranks = $setup->getLvl($user['lvl']);
 
 <body>
 
-    <!--Barre de navigation-->
-    <?php require_once 'components/greenbar.php' ?>
+<!--Barre de navigation-->
+<?php require_once 'components/greenbar.php' ?>
 
-    <!--Block principal-->
-    <div id="main-block">
+<!--Block principal-->
+<div id="main-block">
 
-        <!--Block amis-->
-        <div id="settings-block">
-            <h3>PRÉFÉRENCES DU PROFIL</h3>
+    <!--Block amis-->
+    <div id="settings-block">
+        <h3>PRÉFÉRENCES DU PROFIL</h3>
 
-            <div class="grid">
-                <div class="card">
-                    <p>Photo de profil</p><br>
-                    <p>Image de couverture</p><br>
-                    <p>Identifiant</p><br>
-                    <p>Prénom</p><br>
-                    <p>Nom</p><br>
-                    <p>Date de naissance</p><br>
-                    <p>Âge</p><br>
-                    <p>Pays</p><br>
-                    <p>Jeu favori</p><br>
-                    <p>Genre favori</p><br>
-                    <p>Plateforme favorite</p><br>
-                    <p>Biographie</p>
-                </div>
-                <div class="card">
-                    <form action="#" method="post">
-                        <img class="pp" src="assets/img/UserProfilePicture/<?= $user['img'] ?>" alt="pfp" id="pp" width="100px"><input type="file" name="" value""><br>
-                        <style>
-                            #coverimage {
-                                background-image: linear-gradient(to bottom, transparent 30%, black 150%), url("./images/cover-image-test.jpg");
-                            }
-                        </style>
-                        <div id="coverimage"></div><input type="file" name="" value""><br>
-                        <input type="text" class="" id="" name="pseudo" maxlength="10"><br>
-                        <input type="text" class="" id="" name="prenom" maxlength="10">
-                        <input type="checkbox" id="" name="">
-                        <label for="checkbox">Afficher sur le profil</label>
-                        <br>
-                        <input type="text" class="" id="" name="nom" maxlength="10">
-                        <input type="checkbox" id="" name="">
-                        <label for="checkbox">Afficher sur le profil</label><br>
-                        <input type="date" class="" id="" name="dateNaissance">
-                        <input type="checkbox" id="" name="">
-                        <label for="checkbox">Afficher sur le profil</label><br>
-                        <input type="number" class="" id="" name="age">
-                        <input type="checkbox" id="" name="">
-                        <label for="checkbox">Afficher sur le profil</label><br>
-                        <select name="selectcountry" id="selectcountry">
-                            <option value="">Sélectionner un pays</option>
-                        </select>
-                        <input type="checkbox" id="" name="">
-                        <label for="checkbox">Afficher sur le profil</label><br>
-                        <select name="selectgame" id="selectgame">
-                            <option value="">Sélectionner un jeu</option>
-                        </select><br>
-                        <select name="selectcategory" id="selectcategory">
-                            <option value="">Sélectionner une catégorie</option>
-                            <option value="action">Action</option>
-                            <option value="aventure">Aventure</option>
-                            <option value="horreur">Horreur</option>
-                            <option value="mmo_rpg">RPG / MMO</option>
-                        </select><br>
-                        <select name="selectplateform" id="selectplateform">
-                            <option value="">Sélectionner une plateforme</option>
-                            <option value="pc">PC</option>
-                            <option value="xbox">Xbox</option>
-                            <option value="playstation">PS4 / PS5</option>
-                        </select><br>
-                        <input type="text" class="" id="" name="bio" rows="3" maxlength="100"><br>
-                        <input type="submit" name="btn" value="Valider">
-                    </form>
+        <div class="grid">
+            <div class="card">
+                <p>Photo de profil</p><br>
+                <p>Image de couverture</p><br>
+                <p>Identifiant</p><br>
+                <p>Prénom</p><br>
+                <p>Nom</p><br>
+                <p>Date de naissance</p><br>
+                <p>Âge</p><br>
+                <p>Pays</p><br>
+                <p>Jeu favori</p><br>
+                <p>Genre favori</p><br>
+                <p>Plateforme favorite</p><br>
+                <p>Biographie</p>
+            </div>
+            <div class="card">
+                <form action="#" method="post" enctype="multipart/form-data">
+                    <img class="pp" src="assets/img/UserProfilePicture/<?= $user['img'] ?>" alt="pfp" id="pp"
+                         width="100px"><input type="file" name="" value""><br>
+                    <style>
+                        #coverimage {
+                            background-image: linear-gradient(to bottom, transparent 30%, black 150%), url("./images/cover-image-test.jpg");
+                        }
+                    </style>
+                    <div id="coverimage"></div>
+                    <input type="file" name="img-banner" value""><br>
+                    <input type="text" class="" id="" name="pseudo" maxlength="10"><br>
+                    <input type="text" class="" id="" name="prenom" maxlength="10">
+                    <input type="checkbox" id="" name="">
+                    <label for="checkbox">Afficher sur le profil</label>
+                    <br>
+                    <input type="text" class="" id="" name="nom" maxlength="10">
+                    <input type="checkbox" id="" name="">
+                    <label for="checkbox">Afficher sur le profil</label><br>
+                    <input type="date" class="" id="" name="dateNaissance">
+                    <input type="checkbox" id="" name="">
+                    <label for="checkbox">Afficher sur le profil</label><br>
+                    <input type="number" class="" id="" name="age">
+                    <input type="checkbox" id="" name="">
+                    <label for="checkbox">Afficher sur le profil</label><br>
+                    <select name="selectcountry" id="selectcountry">
+                        <option value="">Sélectionner un pays</option>
+                    </select>
+                    <input type="checkbox" id="" name="">
+                    <label for="checkbox">Afficher sur le profil</label><br>
+                    <select name="selectgame" id="selectgame">
+                        <option value="">Sélectionner un jeu</option>
+                    </select><br>
+                    <select name="selectcategory" id="selectcategory">
+                        <option value="">Sélectionner une catégorie</option>
+                        <option value="action">Action</option>
+                        <option value="aventure">Aventure</option>
+                        <option value="horreur">Horreur</option>
+                        <option value="mmo_rpg">RPG / MMO</option>
+                    </select><br>
+                    <select name="selectplateform" id="selectplateform">
+                        <option value="">Sélectionner une plateforme</option>
+                        <option value="pc">PC</option>
+                        <option value="xbox">Xbox</option>
+                        <option value="playstation">PS4 / PS5</option>
+                    </select><br>
+                    <input type="text" class="" id="" name="bio" rows="3" maxlength="100"><br>
+                    <input type="submit" name="btn" value="Valider">
 
-                </div>
+                    <!-- Back End -->
+                    <?php
+                    if (isset($_POST['btn'])) {
+                        var_dump($_FILES['img-banner']);
+                    }
+                    ?>
+                </form>
             </div>
         </div>
     </div>
+</div>
 </body>
 
 </html>
