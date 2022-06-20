4c162e1b43bbc809b9f9d047ea7731aedb5176da
Tom <tommusique974@gmail.com>
2022-06-20 23:50:11 +0200

    ok

--- pages/profil/jeux.php
+++ pages/profil/jeux.php
@@ -10,6 +10,7 @@ $controler = new controller("localhost", "leveling", "root", "");
 $setup = new setup();
 
 $user = $controler->getUser($_SESSION['id']);
+$userAbout = $controler->getUserAbout($_SESSION['id']);
 $ranks = $setup->getLvl($user['lvl']);
 ?>
 
@@ -107,7 +108,7 @@ $ranks = $setup->getLvl($user['lvl']);
 
     <!--  Jeux fav  -->
     <div class="bottom" style="padding: 20px">
-        <h2 class="sous-title bold">VALORANT</h2>
+        <h2 class="sous-title bold"><?= $userAbout['jeu_fav'] ?></h2>
         <h3 class="header-title bold">269h</h3>
     </div>
     <!--  Jeux fav  -->
