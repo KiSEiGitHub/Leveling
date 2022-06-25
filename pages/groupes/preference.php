144aadde90c992b78b53d49d9c77b0332d1eddca
Tom <tommusique974@gmail.com>
2022-06-25 02:02:53 +0200

    oui

--- pages/groupes/preference.php
+++ pages/groupes/preference.php
@@ -40,7 +40,7 @@ if (isset($_POST['btn'])) {
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../scss/styles.css">
+
 
     <title>Préférences du compte</title>
 </head>
