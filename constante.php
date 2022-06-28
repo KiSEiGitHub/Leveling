51974306a5fb109f0e1cd62c53e35bfd5c069c54
Tom <85096984+KiSEiGitHub@users.noreply.github.com>
2022-06-28 18:57:17 +0200

    Update constante.php

--- constante.php
+++ constante.php
@@ -1,6 +1,6 @@
 <?php
 // ne toucher pas se fichier
-const DB = 'leveling';
-const HOST = 'localhost';
-const USER = 'root';
-const MDP = '';
+const DB = 'heroku_dbd048d49238995';
+const HOST = 'eu-cdbr-west-02.cleardb.net';
+const USER = 'bc62887c818764';
+const MDP = '1cddc47a';
